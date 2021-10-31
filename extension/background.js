bf19f35dcdab4b3a41f5ecc0ac310646375e4982
kazhashimoto <zkar.h62@gmail.com>
2021-10-31 17:48:20 +0900

    Insert CSS file from scripting API. Delete declarativeContent from permissions.

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,8 @@
 chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.insertCSS({
+    target: { tabId: tab.id },
+    files: ['charcheck.css']
+  });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ['charcheck.js']
