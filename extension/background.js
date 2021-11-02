78c5b5eec5018f455daeac042c84e5aad10f91b3
kazhashimoto <zkar.h62@gmail.com>
2021-11-02 10:58:10 +0900

    Add state on action button.

--- extension/background.js
+++ extension/background.js
@@ -7,4 +7,18 @@ chrome.action.onClicked.addListener((tab) => {
     target: { tabId: tab.id },
     files: ['charcheck.js']
   });
+  chrome.action.onClicked.addListener(
+    () => {
+      toggleBadgeText();
+   }
+  );
+  toggleBadgeText();
+
+  function toggleBadgeText() {
+    chrome.action.getBadgeText({tabId: tab.id})
+      .then(result => {
+        const state = (result == 'ON')? 'OFF': 'ON';
+        chrome.action.setBadgeText({text: state});
+      });
+  }
 });
